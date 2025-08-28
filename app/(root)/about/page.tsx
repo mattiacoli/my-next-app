679ceb046887c7daa0e6b0cf62d23d77d07044ed
mattiacoli <mattia.coli@aol.com>
2025-08-28 15:53:28 +0200

    Add About page content and implement error handling components

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,3 +1,10 @@
 export default function About() {
-  return <h1 className="text-3xl">About Us</h1>;
+  // throw new Error("Not implemented");
+
+  return (
+    <>
+      <h1 className="text-3xl">About Us</h1>
+      <p>This is the about page.</p>
+    </>
+  );
 }
